b42942e09d8bfa918187ea1a4acb1e8d6adefb7d
Doğan Kutbay <doankutbay@hotmail.com>
2025-12-14 01:17:39 +0300

    Add Vercel Speed Insights for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import { AppRouter } from './AppRouter.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppRouter />
+    <SpeedInsights />
   </StrictMode>
 )
